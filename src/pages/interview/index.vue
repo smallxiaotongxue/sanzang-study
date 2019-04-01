e4197f69579cd9bf4c7927b437af291e08c2f79a
smallxiaotongxue <583883731@qq.com>
2019-04-01 12:28:33 +0800

    my ---static

--- src/pages/interview/index.vue
+++ src/pages/interview/index.vue
@@ -1,13 +1,54 @@
 <template>
   <div>
-    <h1> 面试题页面 </h1>
+    <div class="question-bg"></div>
+    <div class="question">
+      <div class="question-content">
+        <swiper style="height:100vh" :current="current">
+          <swiper-item>
+            <scroll-view scroll-y="true" style="height: 100%">
+              <div class="question-name">
+                <div class="text">CSS的Scoped私有作用域和深度选择器???</div>
+              </div>
+              <div class="answer-options">
+                <ul class="options-list">
+                  <li class="list-item">
+                    <div class="option-btn">xxxxxxxxxx</div>
+                  </li>
+                  <li class="list-item">
+                    <div class="option-btn">xxxxxxxxxx</div>
+                  </li>
+                  <li class="list-item">
+                    <div class="option-btn">xxxxxxxxxx</div>
+                  </li>
+                  <li class="list-item">
+                    <div class="option-btn">xxxxxxxxxx</div>
+                  </li>
+                </ul>
+              </div>
+            </scroll-view>
+          </swiper-item>
+        </swiper>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   data: function () {
-    return {}
+    return {
+      currentNum: 6,
+      allNum: 10
+    }
+  },
+  computed: {
+    answerPrecent () {
+      return this.currentNum / this.allNum * 100 || ''
+    },
+    precentStatus () {
+      return this.readPrecent === 100 ? 'success' : 'active'
+    }
   },
   components: {},
   methods: {},
@@ -16,6 +57,98 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 
+.question-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1000;
+  background: linear-gradient(to bottom, #1EB1F7, #9be2ff)
+}
+
+.question {
+
+  .answer-progress {
+    display: flex;
+    align-items: center;
+    min-height: 80rpx;
+    padding: 20rpx;
+
+    .progress-wrap {
+      flex: 1;
+      height: 40rpx;
+      padding: 0 10rpx;
+      border-radius: 50rpx;
+      background: #fff;
+
+      .progress-position {
+        margin-top: -6px;
+      }
+    }
+
+    .progress-tips {
+      width: 150rpx;
+      margin-left: 10rpx;
+      color: #fff;
+      text-align: center;
+
+      .num {
+        font-size: 16px;
+        font-weight: bold;
+      }
+
+      .text {
+        font-size: 12px;
+      }
+    }
+  }
+
+  .question-content {
+
+    .question-name {
+      min-height: 350rpx;
+      color: #fff;
+      font-size: 18px;
+      font-weight: bold;
+      padding: 10rpx 30rpx;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .text {
+        word-break: break-all;
+      }
+    }
+
+    .answer-options {
+
+      .options-list {
+        padding: 30rpx;
+
+        .list-item {
+          margin: 50rpx;
+        }
+      }
+    }
+
+    .option-btn {
+      padding: 10rpx 20rpx;
+      min-height: 70rpx;
+      line-height: 70rpx;
+      font-size: 14px;
+      text-align: center;
+      transition: all .3s linear;
+      color: #fff;
+      border: 1px solid #fff;
+      border-radius: 50rpx;
+
+      &:active {
+        background: #9be2ff;
+      }
+    }
+  }
+}
 </style>
---
+++ src/pages/my/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="my">
+    <div class="my-info">
+      <img @click="toLogin" :src="avator" alt="">
+      <div class="info__text" @click="toLogin">
+        <p>{{userInfo.nickName}}</p>
+        <p v-if="!userInfo.nickname">点击登录</p>
+        <p v-else>微信用户</p>
+      </div>
+    </div>
+
+    <ul class="my-list">
+      <li class="list-item">
+        <div>推荐给朋友</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  onShow () {},
+  created () {},
+  mounted () {},
+  data () {
+    return {
+      avator: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png',
+      allcheck: false,
+      Listids: [],
+      userInfo: {}
+    }
+  },
+  components: {},
+  methods: {
+    toLogin () {}
+  },
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss">
+
+.my {
+  background: #f3f3f3;
+  padding-bottom: 1rpx;
+
+  .my-info {
+    width: 100%;
+    height: 250rpx;
+    display: flex;
+    align-items: center;
+    background: #333;
+    padding: 0 30rpx;
+    box-sizing: border-box;
+
+    .info__text {
+      margin-left: 30rpx;
+      p {
+        color: #fff;
+        font-size: 16px;
+        margin-bottom: 10rpx
+      }
+      p:nth-child(2) {
+        font-size: 14px;
+      }
+    }
+  }
+
+  img {
+    height: 120rpx;
+    width: 120rpx;
+    border-radius: 50%
+  }
+
+  .my-list {
+    margin: 20rpx 0;
+
+    .list-item {
+      display: flex;
+      align-items: center;
+      background: #fff;
+      font-size: 14px;
+      min-height: 100rpx;
+      padding: 0 30rpx;
+
+      .item-body {
+
+      }
+    }
+
+  }
+}
+</style>
